eb6c593422fae52e4048cdf177ef8dce2096f1b5
mehdi <m2110ad@gmail.com>
2024-01-08 18:33:03 +0330

    better create-user enpoint : checks if all the parameters exist if not sends a 400 status code

--- main.js
+++ main.js
@@ -11,7 +11,8 @@ const Users = require('./models/User.models');
 
 
 
-const router = require('./routes/tehran.routes')
+const router = require('./routes/tehran.routes');
+const { error } = require('console');
 
 
 app.use((req, res, next) => {
@@ -38,16 +39,33 @@ app.get('/', async (req, resp) => {
 app.post('/create-user', async (req, resp) => {
 
     const { name, lastname, password, email } = req.body
-    const user = await Users.create({
-        firstName: name,
-        lastName: lastname,
-        password: password,
-        email: email
-    });
+    const list = [name, lastname, password, email];
 
-    resp.send(user).status(200)
+    try {
+        const allVarsExist = list.every(element => element !== null && element !== undefined);
+
+        console.log(allVarsExist);
+
+        if (!allVarsExist) { resp.status(400).send("a variable does not exist ") }
+
+        else {
+
+            const user = await Users.create({
+                firstName: name,
+                lastName: lastname,
+                password: password,
+                email: email
+            });
+
+            resp.send(user).status(200)
+        }
+    }
+    catch (error) {
+
+        console.log(error);
+        resp.send(500)
+    }
 })
-
 
 app.listen(port, async () => {
     try {
