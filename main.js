54615f465287c86013987da3834e8d238a2cec9a
mehdi <m2110ad@gmail.com>
2024-01-09 12:35:32 +0330

    login post got added

--- main.js
+++ main.js
@@ -18,6 +18,7 @@ app.use((req, res, next) => {
     console.log("cache is disabled")
     next();
 });
+
 app.use(bodyParser.json()) // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
@@ -56,7 +57,6 @@ app.post('/create-user', async (req, resp) => {
             resp.send(user).status(200)
         }
     }
-
     catch (error) {
 
         console.log(error);
@@ -64,7 +64,67 @@ app.post('/create-user', async (req, resp) => {
     }
 })
 
+app.post("/login", async (req, resp) => {
+
+    const { email, password } = req.body;
+
+    const list = [email, password]
+
+    console.log(req.body)
+
+    const variablesExist = list.every(element => element !== null && element !== undefined)
+
+    console.log("variablesExist", variablesExist)
+
+    try {
+        if (!variablesExist) {
+
+            resp.status(400).send("bad request")
+        }
+        else {
+
+            const user = await Users.findOne({
+
+                where: {
+
+                    email
+                }
+            })
+            if (user) {
+
+                if (user.password === password) {
+
+                    console.log("password matches")
+                    resp.status(200).send(`welcome to panel ${user.firstName}`);
+
+                }
+                else if (user.password !== password) {
+
+                    console.log("password does not match")
+                    resp.status(401).send("wrong credentials");
+                }
+
+                else {
+                    console.log("error from inside if")
+                }
+            }
+
+            else {
+
+                resp.status(200).send("user's not found!!!")
+
+            }
+        }
+    }
+    catch (error) {
+
+        resp.status(500).send("internal server error")
+
+    }
+})
+
 const port = 8888;
+
 app.listen(port, async () => {
     try {
         await sequelize.authenticate();
